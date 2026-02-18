43cd8a509b20d204c307993cf86338f84730b398
SupakornThongaerd <supakorn642@gmail.com>
2026-02-19 01:15:07 +0700

    feat: add lib usequery help cache video

--- src/app/(public)/tgds/assessment/page.tsx
+++ src/app/(public)/tgds/assessment/page.tsx
@@ -4,20 +4,12 @@ import { useRouter } from 'next/navigation'
 import { AppShell } from '@/components/public/AppShell'
 import { AssessmentMoodTGDSPage } from '@/components/public/AssessmentMoodTGDSPage'
 import { useAssessmentStore } from '@/store/assessmentStore'
-import { useEffect } from 'react'
-import { startTGDS } from '@/api/tgds/startTGDS'
 import { completeTGDS } from '@/api/tgds/completeTGDS'
 
 export default function TGDSAssessmentRoute() {
   const router = useRouter()
   const setMoodScore = useAssessmentStore((s) => s.setMoodScore)
   const sessionId = useAssessmentStore((s) => s.sessionId)
-  useEffect(() => {
-      if (!sessionId) return
-
-      // fire-and-forget
-      startTGDS({ session_id: sessionId })
-  }, [sessionId])
 
   return (
     <AppShell>
--- src/components/admin/useTgds.tsx
+++ src/components/admin/useTgds.tsx
@@ -1,34 +1,14 @@
-'use client'
-
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import type { TgdsDetail } from '@/types/tgds'
 
 export function useTgds(sessionId: string) {
-  const [tgds, setTgds] = useState<TgdsDetail | null>(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
-
-  useEffect(() => {
-    const fetchTgds = async () => {
-      try {
-        const res = await fetch(`/api/dashboard/tgds/${sessionId}`, {
-          cache: 'no-store',
-        })
-
-        if (!res.ok) throw new Error()
-
-        const data: TgdsDetail = await res.json()
-        console.log(data)
-        setTgds(data)
-      } catch {
-        setError(true)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchTgds()
-  }, [sessionId])
-
-  return { tgds, loading, error }
+  return useQuery({
+    queryKey: ['tgds', sessionId],
+    queryFn: async () => {
+      const res = await fetch(`/api/dashboard/tgds/${sessionId}`)
+      if (!res.ok) throw new Error()
+      return res.json() as Promise<TgdsDetail>
+    },
+    staleTime: 1000 * 60 * 5, 
+  })
 }
