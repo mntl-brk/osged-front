2389b50fc9f2ea342a4379d76ad7f173a44b25c6
SupakornThongaerd <supakorn642@gmail.com>
2026-03-04 21:17:39 +0700

    refactor: api response media

--- src/app/api/media/video_audio/[...path]/route.ts
+++ src/app/api/media/video_audio/[...path]/route.ts
@@ -4,10 +4,10 @@ export async function GET(
   req: NextRequest,
   context: { params: Promise<{ path: string[] }> }
 ) {
-
   const { path } = await context.params
-
   const filePath = path.join('/')
+
+  const range = req.headers.get('range');
 
   const backendRes = await fetch(
     `${process.env.BACKEND_API_URL}/doctor/video_audio/${filePath}`,
@@ -15,18 +15,31 @@ export async function GET(
       headers: {
         'CF-Access-Client-Id': process.env.CF_ACCESS_CLIENT_ID!,
         'CF-Access-Client-Secret': process.env.CF_ACCESS_CLIENT_SECRET!,
-        range: req.headers.get('range') || '',
+        ...(range && { range }),
       },
     }
   )
 
+  if (!backendRes.ok && backendRes.status !== 206) {
+    return new NextResponse("Error loading media", { status: backendRes.status });
+  }
+
+  const responseHeaders = new Headers();
+    const headersToForward = [
+    'content-type',
+    'content-range',
+    'accept-ranges',
+    'content-length',
+    'cache-control'
+  ];
+
+  headersToForward.forEach(header => {
+    const value = backendRes.headers.get(header);
+    if (value) responseHeaders.set(header, value);
+  });
+
   return new NextResponse(backendRes.body, {
     status: backendRes.status,
-    headers: {
-      'Content-Type': backendRes.headers.get('content-type') || '',
-      'Content-Range': backendRes.headers.get('content-range') || '',
-      'Accept-Ranges': backendRes.headers.get('accept-ranges') || '',
-      'Content-Length': backendRes.headers.get('content-length') || '',
-    },
+    headers: responseHeaders,
   })
 }
